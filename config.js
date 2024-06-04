96b13767c19c3c1b489433e3fd5dabdbebd10f60
nikolan <94351312+nikolan123@users.noreply.github.com>
2024-06-04 12:03:37 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,20 @@
 const host = '0.0.0.0'
-const port = 25565
-const version = '1.16.3'
 
-const abuseipdb_key = process.env["ABUSEIPDB_API_KEY"];
+const port = 25565
+const version = '1.20.4'
+const motd = "meow"
+
+const abuseipdb_key = "";
 const report_ip = true;
 
-const discord_webhook = process.env["DISCORD_WEBHOOK_URL"];
+const discord_webhook = "";
 const send_to_webhook = true;
 
 module.exports = {
     host,
     port,
     version,
+    motd,
     abuseipdb_key,
     report_ip,
     discord_webhook,
